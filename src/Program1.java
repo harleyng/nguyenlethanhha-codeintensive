ed0a2ae6eea19cf3020f1648c9495b406877f5ee
Thanh Ha Ng Le <harley.ngx@gmail.com>
2019-02-27 19:22:39 +0700

    initial commit

---
+++ src/Program1.java
@@ -0,0 +1,40 @@
+/**
+ * Created by huynq on 7/4/17.
+ */
+public class Program1 {
+//    static  double[] equation2(double a, double b, double c) {
+//        double[] result = new double[2];
+//        // TODO
+//        if (a == 0) {
+//            if (b == 0) {
+//                result = null;
+//            } else {
+//                result[0] = -c/b;
+//                result[1] = -c/b;
+//            }
+//        } else  {
+//            double delta = b*b - 4*a*c;
+//            if (delta < 0) {
+//                result = null;
+//            } else if (delta == 0) {
+//                result[0] = -b / 2 / a;
+//                result[1] = -b / 2 / a;
+//
+//            } else if (delta > 0) {
+//                result[0] = (-b + Math.sqrt(delta)) / 2 / a;
+//                result[1]= (-b - Math.sqrt(delta)) / 2 / a;
+//
+//            }
+//        }
+//
+//        return result;
+//    }
+    public static void main(String[] args) {
+//        double[] result = equation2(1, 2, 1);
+//        if (result == null) {
+//            System.out.println("Can not resolve");
+//        } else {
+//            System.out.println("x1 = " + result[0] + "; x2 = " + result[0]);
+//        }
+    }
+}
